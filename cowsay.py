920ea4f1ba67fda217e1deea7504e90b7c51b113
Satou-art <57704808+Satou-art@users.noreply.github.com>
2020-05-31 03:31:05 +0430

    cow inserted

--- cowsay.py
+++ cowsay.py
@@ -5,7 +5,12 @@ from time import sleep
 def main():
     def cowsay(som):   
         s = " " + (len(som) + 2) * "_" + " \n" + "(" + (len(som) + 2) * " " + ")\n" + "( " + som + " )\n" + "(" + (len(som) + 2) * " " + ")\n" + " " + (len(som) + 2) * "-"
-        print(s)
+        cow = f"""{len(som)*" "}\\   ^__^
+{len(som)*" "}  \\  (oo)\________
+{len(som)*" "}     (__)\        )\\/\\
+{len(som)*" "}         ||-----w |
+{len(som)*" "}         ||      ||\n"""
+        print(s, "\n",  cow)
     cowsay(input())
     sleep(2.5)
 
